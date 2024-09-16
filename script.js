ed2dccb728ccf9e4261b991e9d919775d0128fa0
omarboury3 <bouryomar3@gmail.com>
2024-09-16 20:48:05 +0200

    PROYECTO FINALIZADO RANDOM COLOR

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 
-const titulo = document.querySelector(".titulo");
+
+// Inicializamos las variables constantes de los elementos necesarios
+const body = document.getElementsByTagName('body');
+const contenedor = document.getElementsByClassName('contenedor');
+const textoHexaDecimal = document.getElementsByClassName('texto-hexadecimal');
 
 
-titulo.style.color = 'blue';
+// Creamos la funcion que genera el color random para cambiar de color el body
+function getRandomColor() {
 
+    return  ('#' + Math.floor((Math.random() * 16777215)).toString(16));
+}
 
-titulo.innerText = "XD";
+// Creamos la función para que al clicar el boton se cambie el bolor del background y genere el codigo del color
+
+function clickRandomColor() {
+
+    body[0].style.backgroundColor = getRandomColor();
+    contenedor[0].style.backgroundColor = 'white';
+
+    textoHexaDecimal[0].innerText = getRandomColor();
+
+}
+
